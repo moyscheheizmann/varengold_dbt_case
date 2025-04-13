440cdde403f8ac408b804316f9ab5e0aa5b1ee2d
Victor Staack <victor.staack@proton.me>
2025-04-13 17:31:42 +0000

    Enhance staging models by adding date parsing, error flags, and improved descriptions for columns

--- transformation/models/staging/stg_raw_staging__customers.sql
+++ transformation/models/staging/stg_raw_staging__customers.sql
@@ -1,19 +1,15 @@
 with source as (
-
     select * from {{ source('raw', 'customers') }}
-
 ),
 
 renamed as (
-
-    select
+    select distinct
         customer_id,
-        firstname,
-        lastname,
-        Age as 'age'
-
+        trim(firstname) as firstname,
+        trim(lastname) as lastname,
+        Age as age,
+        branch_id
     from source
-
 )
 
 select * from renamed
